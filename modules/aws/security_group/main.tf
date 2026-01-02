d56eb6b8f3308809b3d4e0dac6c53fb9f3782e64
skawashi <skawashi.engineer@gmail.com>
2026-01-03 06:17:43 +0900

    import security_group nat

--- modules/aws/security_group/main.tf
+++ modules/aws/security_group/main.tf
@@ -18,3 +18,36 @@ resource "aws_security_group" "bastion" {
   }
 }
 
+resource "aws_security_group" "nat" {
+  region      = "ap-northeast-1"
+  vpc_id      = var.vpc_id
+  name        = "cp-nat-${var.env}"
+  description = "Managed by Terraform"
+
+  egress = [{
+    cidr_blocks      = ["0.0.0.0/0"]
+    description      = ""
+    from_port        = 0
+    ipv6_cidr_blocks = []
+    prefix_list_ids  = []
+    protocol         = "-1"
+    security_groups  = []
+    self             = false
+    to_port          = 0
+  }]
+
+  ingress = [{
+    cidr_blocks      = ["10.0.128.0/18", "10.0.192.0/18"]
+    description      = "Allow traffic from private subnets"
+    from_port        = 0
+    ipv6_cidr_blocks = []
+    prefix_list_ids  = []
+    protocol         = "-1"
+    security_groups  = []
+    self             = false
+    to_port          = 0
+  }]
+  tags = {
+    Name = "cp-nat-${var.env}"
+  }
+}
