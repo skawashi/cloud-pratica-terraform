4eec6f152af263cbb34a784160f410eccd69edc6
skawashi <skawashi.engineer@gmail.com>
2026-01-03 06:24:32 +0900

    fix security_group alb_cp, bastion's egress and ingress

--- modules/aws/security_group/main.tf
+++ modules/aws/security_group/main.tf
@@ -3,27 +3,6 @@ resource "aws_security_group" "alb_cp" {
   vpc_id      = var.vpc_id
   name        = "cp-alb-${var.env}"
   description = "Managed by Terraform"
-  tags = {
-    Name = "cp-alb-${var.env}"
-  }
-}
-
-resource "aws_security_group" "bastion" {
-  region      = "ap-northeast-1"
-  vpc_id      = var.vpc_id
-  name        = "cp-bastion-${var.env}"
-  description = "Managed by Terraform"
-  tags = {
-    Name = "cp-bastion-${var.env}"
-  }
-}
-
-resource "aws_security_group" "nat" {
-  region      = "ap-northeast-1"
-  vpc_id      = var.vpc_id
-  name        = "cp-nat-${var.env}"
-  description = "Managed by Terraform"
-
   egress = [{
     cidr_blocks      = ["0.0.0.0/0"]
     description      = ""
@@ -35,7 +14,61 @@ resource "aws_security_group" "nat" {
     self             = false
     to_port          = 0
   }]
+  ingress = [{
+    cidr_blocks      = ["0.0.0.0/0"]
+    description      = ""
+    from_port        = 443
+    ipv6_cidr_blocks = []
+    prefix_list_ids  = []
+    protocol         = "tcp"
+    security_groups  = []
+    self             = false
+    to_port          = 443
+  }]
+  tags = {
+    Name = "cp-alb-${var.env}"
+  }
+}
 
+resource "aws_security_group" "bastion" {
+  region      = "ap-northeast-1"
+  vpc_id      = var.vpc_id
+  name        = "cp-bastion-${var.env}"
+  description = "Managed by Terraform"
+  egress = [{
+    cidr_blocks      = ["0.0.0.0/0"]
+    description      = ""
+    from_port        = 0
+    ipv6_cidr_blocks = []
+    prefix_list_ids  = []
+    protocol         = "-1"
+    security_groups  = []
+    self             = false
+    to_port          = 0
+  }]
+  ingress = []
+
+  tags = {
+    Name = "cp-bastion-${var.env}"
+  }
+}
+
+resource "aws_security_group" "nat" {
+  region      = "ap-northeast-1"
+  vpc_id      = var.vpc_id
+  name        = "cp-nat-${var.env}"
+  description = "Managed by Terraform"
+  egress = [{
+    cidr_blocks      = ["0.0.0.0/0"]
+    description      = ""
+    from_port        = 0
+    ipv6_cidr_blocks = []
+    prefix_list_ids  = []
+    protocol         = "-1"
+    security_groups  = []
+    self             = false
+    to_port          = 0
+  }]
   ingress = [{
     cidr_blocks      = ["10.0.128.0/18", "10.0.192.0/18"]
     description      = "Allow traffic from private subnets"
