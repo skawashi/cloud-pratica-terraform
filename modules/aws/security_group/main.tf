8cf723951401140893bfada44618804aa8baa849
skawashi <skawashi.engineer@gmail.com>
2026-01-03 06:50:57 +0900

    import security_group db

--- modules/aws/security_group/main.tf
+++ modules/aws/security_group/main.tf
@@ -138,3 +138,40 @@ resource "aws_security_group" "db_migrator" {
     Name = "cp-db-migrator-${var.env}"
   }
 }
+
+resource "aws_security_group" "db" {
+  region      = "ap-northeast-1"
+  vpc_id      = var.vpc_id
+  name        = "cp-db-${var.env}"
+  description = "Managed by Terraform"
+  egress = [{
+    cidr_blocks      = ["0.0.0.0/0"]
+    description      = ""
+    from_port        = 0
+    ipv6_cidr_blocks = []
+    prefix_list_ids  = []
+    protocol         = "-1"
+    security_groups  = []
+    self             = false
+    to_port          = 0
+  }]
+  ingress = [{
+    cidr_blocks      = []
+    description      = ""
+    from_port        = 5432
+    ipv6_cidr_blocks = []
+    prefix_list_ids  = []
+    protocol         = "tcp"
+    security_groups = [
+      aws_security_group.bastion.id,
+      aws_security_group.slack_metrics_backend.id,
+      aws_security_group.db_migrator.id,
+    ]
+    self    = false
+    to_port = 5432
+  }]
+
+  tags = {
+    Name = "cp-db-${var.env}"
+  }
+}
