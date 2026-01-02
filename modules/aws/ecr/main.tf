654956bde5844947ba56d14743205f4d6f486e0c
skawashi <skawashi.engineer@gmail.com>
2026-01-03 08:14:52 +0900

    import ecr_lifecycle_policy db_migrator

--- modules/aws/ecr/main.tf
+++ modules/aws/ecr/main.tf
@@ -9,3 +9,24 @@ resource "aws_ecr_repository" "db_migrator" {
   region               = "ap-northeast-1"
   image_tag_mutability = var.image_tag_mutability
 }
+
+resource "aws_ecr_lifecycle_policy" "db_migrator" {
+  repository = aws_ecr_repository.db_migrator.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "最新の3世代のイメージのみを保持"
+        selection = {
+          tagStatus   = "any"
+          countType   = "imageCountMoreThan"
+          countNumber = 3
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
