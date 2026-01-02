fbf3b8b7b06d22b763b65f4b44a51ac1dede5220
skawashi <skawashi.engineer@gmail.com>
2026-01-03 06:09:04 +0900

    import security_group bastion

--- modules/aws/security_group/main.tf
+++ modules/aws/security_group/main.tf
@@ -3,21 +3,18 @@ resource "aws_security_group" "alb_cp" {
   vpc_id      = var.vpc_id
   name        = "cp-alb-${var.env}"
   description = "Managed by Terraform"
-
-  ingress = [{
-    cidr_blocks      = ["0.0.0.0/0"]
-    description      = ""
-    from_port        = 443
-    ipv6_cidr_blocks = []
-    prefix_list_ids  = []
-    protocol         = "tcp"
-    security_groups  = []
-    self             = false
-    to_port          = 443
-  }]
-
   tags = {
     Name = "cp-alb-${var.env}"
+  }
+}
+
+resource "aws_security_group" "bastion" {
+  region      = "ap-northeast-1"
+  vpc_id      = var.vpc_id
+  name        = "cp-bastion-${var.env}"
+  description = "Managed by Terraform"
+  tags = {
+    Name = "cp-bastion-${var.env}"
   }
 }
 
