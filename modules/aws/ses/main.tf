55febdf162c2b43b002bcfd2f3970305861953c0
skawashi <skawashi.engineer@gmail.com>
2026-01-03 22:51:21 +0900

    import ses email_identity_mail_from_attributes cloud-pratica

--- modules/aws/ses/main.tf
+++ modules/aws/ses/main.tf
@@ -1,3 +1,8 @@
 resource "aws_sesv2_email_identity" "cloud_pratica" {
-  email_identity = "stg.cloud-pratica.kawashima.world"
+  email_identity = "${var.env}.cloud-pratica.kawashima.world" // TODO domain設定
+}
+
+resource "aws_sesv2_email_identity_mail_from_attributes" "cloud_pratica" {
+  email_identity   = "${var.env}.cloud-pratica.kawashima.world"      // TODO domain設定
+  mail_from_domain = "mail.${var.env}.cloud-pratica.kawashima.world" // TODO domain設定
 }
